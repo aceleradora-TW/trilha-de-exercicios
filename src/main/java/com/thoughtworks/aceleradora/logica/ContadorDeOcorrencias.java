407ae6bc2ffa11da1f2eec82a00db851ff3e69d9
cassia11 <cassiagomes0102@gmail.com>
2019-03-27 15:31:48 -0300

    Faz exercicio contador de ocorrencias

--- src/main/java/com/thoughtworks/aceleradora/logica/ContadorDeOcorrencias.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/ContadorDeOcorrencias.java
@@ -1,11 +1,15 @@
 package com.thoughtworks.aceleradora.logica;
 
 public class ContadorDeOcorrencias {
-
-    public int contaOcorrencias(String texto, String token) {
-
-        return -1;
+    public int contaOcorrencias(String texto, String busca) {
+        int cont = 0;
+        for (int i = 0; i < texto.length(); i++) {
+            if (busca.contentEquals(String.valueOf(texto.charAt(i)))){
+                cont = cont + 1;
+            }
+        }
+        return cont;
     }
 
-
 }
+
