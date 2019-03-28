e1a4356c348e3d550364dcae53f10ca92cf7dd5b
cassia11 <cassiagomes0102@gmail.com>
2019-03-28 15:51:30 -0300

    tira linhas comentadas

--- src/main/java/com/thoughtworks/aceleradora/oo/conversor/Testes.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/conversor/Testes.java
@@ -6,39 +6,5 @@ import java.util.Scanner;
 
 public class Testes {
 
-//    public static void main(String[] args) {
-//        final List<TaxaCambio> TAXAS_DISPONIVEIS = Arrays.asList(
-//                new TaxaCambio(Moeda.REAL_BRASILEIRO, 175.65, Moeda.PESO_CHILENO),
-//                new TaxaCambio(Moeda.REAL_BRASILEIRO, 0.23, Moeda.EURO),
-//
-//                new TaxaCambio(Moeda.EURO, 754.01, Moeda.PESO_CHILENO),
-//                new TaxaCambio(Moeda.EURO, 4.29, Moeda.REAL_BRASILEIRO),
-//
-//                new TaxaCambio(Moeda.PESO_CHILENO, 0.0013, Moeda.EURO),
-//                new TaxaCambio(Moeda.PESO_CHILENO, 0.0057, Moeda.REAL_BRASILEIRO));
-//
-//
-
-//
-//        double valor = conversor.retornaValor(entrada);
-//
-//        String primeiraMoeda = conversor.moedaOriginal(entrada);
-//
-//        String segundaMoeda = conversor.moedaDestino(entrada);
-//
-//        double resultado = 0;
-//
-//        for (TaxaCambio taxa : TAXAS_DISPONIVEIS){
-//
-//            if(primeiraMoeda.equalsIgnoreCase(taxa.getMoedaDeOrigem().getCodigo())){
-//                if(segundaMoeda.equalsIgnoreCase(taxa.getMoedaDeDestino().getCodigo())){
-//                    resultado = valor * taxa.getTaxa();
-//
-//
-//                }
-//            }
-//        }
-//        System.out.println(resultado);
-//    }
 }
 
