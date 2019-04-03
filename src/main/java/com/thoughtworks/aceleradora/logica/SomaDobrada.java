51db4a27e7fe72a332c15a30e6ebe89524fc837e
DaniiNyan <daniela.dbass@gmail.com>
2019-04-03 13:51:30 -0300

    <@daniinyan> resolve trilha de exercicios

--- src/main/java/com/thoughtworks/aceleradora/logica/SomaDobrada.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/SomaDobrada.java
@@ -1,8 +1,20 @@
 package com.thoughtworks.aceleradora.logica;
 
+import org.assertj.core.internal.bytebuddy.asm.Advice;
+
 public class SomaDobrada {
 
     public int soma(int umValor, int outroValor) {
-       return -500;
+
+        if (umValor == outroValor){
+            return 2*(umValor + outroValor);
+        }
+
+        if (umValor < 1 || outroValor < 1){
+            return -1;
+        }
+
+        return umValor + outroValor;
+
     }
 }
---
+++ src/main/java/com/thoughtworks/aceleradora/oo/conversor/Moeda.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.aceleradora.oo.conversor;
+
+public class Moeda {
+    private String nome;
+    private Double valor;
+    private Double taxaBRL;
+    private Double taxaCLP;
+    private Double taxaEUR;
+
+
+
+}
