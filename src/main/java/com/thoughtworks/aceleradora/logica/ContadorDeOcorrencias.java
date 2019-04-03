932af81def80f8bbbbaf5c905da082df2ac05db3
cassia11 <cassiagomes0102@gmail.com>
2019-04-03 13:31:27 -0300

    <@cassia11, @jurley-ribeiro>Faz alguns exercicios da trilha de exercicios

--- src/main/java/com/thoughtworks/aceleradora/logica/ContadorDeOcorrencias.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/ContadorDeOcorrencias.java
@@ -1,9 +1,21 @@
 package com.thoughtworks.aceleradora.logica;
 
+
 public class ContadorDeOcorrencias {
 
     public int contaOcorrencias(String texto, String token) {
-        return -1;
-    }
+        char token2 = token.charAt(0);
+        int contToken=0;
 
+        char[] letras = texto.toCharArray();
+
+        for(int i=0; i < letras.length; i++ ){
+            if(letras[i] == token2){
+               contToken++;
+            }
+
+        }
+        return contToken;
+
+    }
 }
