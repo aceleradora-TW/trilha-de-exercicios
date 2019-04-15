214534d9b1b1920a5baa2fec07837c4fc82a9399
cassia11 <cassiagomes0102@gmail.com>
2019-04-15 14:11:23 -0300

    <@barbaraAnger, @cassia11>Ajuste exercicio biblioteca

--- src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Autor.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Autor.java
@@ -7,6 +7,9 @@ public class Autor {
     private String nome;
     private String sobrenome;
 
+    public Autor(){
+
+    }
     public Autor(String nome, String sobrenome) {
         this.nome = nome;
         this.sobrenome = sobrenome;
@@ -18,6 +21,14 @@ public class Autor {
 
     public String getSobrenome() {
         return sobrenome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
     }
 
     public String toString() {
