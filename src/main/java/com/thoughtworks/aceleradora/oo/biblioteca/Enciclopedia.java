25dc2a7550a7a35837ed02609345b39d83eaf6e6
cassia11 <cassiagomes0102@gmail.com>
2019-04-15 14:00:33 -0300

    <@barbaraAnger, @cassia11>Faz parte do exercicio biblioteca

--- src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Enciclopedia.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Enciclopedia.java
@@ -8,7 +8,17 @@ public class Enciclopedia {
     private List<Livro> livros = new ArrayList<>();
 
     public Enciclopedia() {
-        // Adicione os livros aqui
+        Livro livro0 = new Livro();
+        Autor autor0 = new Autor();
+        ArrayList<Formato> formatos= new ArrayList<Formato>();
+        formatos.add(Formato.EBOOK);
+        livro0.setTitulo("Sejamos todos feministas");
+        autor0.setNome("Chimamanda");
+        autor0.setSobrenome("Adichie");
+        livro0.setAnoDeLancamento(2014);
+        livro0.setFormatos(formatos);
+        livros.add(livro0);
+
     }
 
     public String tituloDoEnesimoLivro(int posicao) {
