e9b99ac4c7340dfdee8968e7031945865d0de987
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-08-08 16:02:22 -0300

    @BrayanArgradem:Faz exercicio do Armstrong

--- src/main/java/com/thoughtworks/aceleradora/logica/IdentificadorDeArmstrong.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/IdentificadorDeArmstrong.java
@@ -3,6 +3,22 @@ package com.thoughtworks.aceleradora.logica;
 public class IdentificadorDeArmstrong {
 
     public String identificaNumero(int numero) {
-        return "Eu ainda nao sei identificar numeros de Armstrong";
+
+        String numeroString = String.valueOf(numero);
+        int tamanhoString = numeroString.length();
+
+        int resultado = 0;
+        int aux = 0;
+        for (int i = 0; i < tamanhoString; i++) {
+            resultado += Math.pow(Integer.parseInt(String.valueOf(numeroString.charAt(i))), tamanhoString);
+        }
+
+        if (resultado == numero) {
+            return "Este eh um numero de Armstrong!";
+
+        } else {
+            return "Este nao eh um numero de Armstrong!";
+
+        }
     }
 }
