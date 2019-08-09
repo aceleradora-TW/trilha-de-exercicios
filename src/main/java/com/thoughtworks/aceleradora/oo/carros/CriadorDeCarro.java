32c63ff6f68cb10c3ae8487e6f1e44983a18ba85
cassia11 <ingrid.b.duarte.id.id@gmail.com>
2019-08-09 15:05:54 -0300

    @BrayanArgradem: Faz exercicios de oo carros

--- src/main/java/com/thoughtworks/aceleradora/oo/carros/CriadorDeCarro.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/carros/CriadorDeCarro.java
@@ -3,6 +3,11 @@ package com.thoughtworks.aceleradora.oo.carros;
 public class CriadorDeCarro {
 
     public Carro criaUmCarro() {
-        return null;
+        Carro fusca = new Carro();
+
+        fusca.setMarca("Ford");
+        fusca.setModelo("Mustang");
+        fusca.setAno(2017);
+        return fusca;
     }
 }
