40a8628c27dc3f47207f78ff6f348a000a670773
FranciscoGFerreira <franciscogabriellferreira@gmail.com>
2019-09-09 11:33:45 -0300

    dojo-16-agil/@todos: faz contador de ocorrencia e faz soma dobrada

--- src/main/java/com/thoughtworks/aceleradora/logica/ContadorDeOcorrencias.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/ContadorDeOcorrencias.java
@@ -3,7 +3,16 @@ package com.thoughtworks.aceleradora.logica;
 public class ContadorDeOcorrencias {
 
     public int contaOcorrencias(String texto, String token) {
-        return -1;
+        int contador = 0;
+
+        String vetor [] = texto.split("");
+
+        for (int i =0; i<vetor.length; i++){
+            if (vetor[i].equals(token)){
+                contador++;
+            }
+        }
+        return contador;
     }
 
 }
