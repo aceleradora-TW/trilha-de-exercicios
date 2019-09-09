e0cb957caf2999630a88d710546e6e5dabf1e1a9
FranciscoGFerreira <franciscogabriellferreira@gmail.com>
2019-09-09 15:31:42 -0300

    dojo-16-agil/@todos: faz o exercício identificador de armstrong

--- src/main/java/com/thoughtworks/aceleradora/logica/IdentificadorDeArmstrong.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/IdentificadorDeArmstrong.java
@@ -1,8 +1,23 @@
 package com.thoughtworks.aceleradora.logica;
 
+import java.util.Objects;
+
 public class IdentificadorDeArmstrong {
 
     public String identificaNumero(int numero) {
-        return "Eu ainda nao sei identificar numeros de Armstrong";
+        String outroNumero = Objects.toString(numero);
+        String[] vet = outroNumero.split("");
+        int tamanho = vet.length;
+        double soma = 0;
+
+        for (int i = 0; i < tamanho; i++) {
+            soma = soma + Math.pow(Integer.parseInt(vet[i]), tamanho);
+        }
+
+        if(soma == numero){
+            return "Este eh um numero de Armstrong!";
+        }
+
+        return "Este nao eh um numero de Armstrong!";
     }
 }
