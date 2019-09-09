a493bdd36f95c459ae2649d112c34f6f632bc659
FranciscoGFerreira <franciscogabriellferreira@gmail.com>
2019-09-09 15:58:56 -0300

    dojo-16-agil/@todos:inicia exercicio carro

--- src/main/java/com/thoughtworks/aceleradora/oo/carros/Carro.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/carros/Carro.java
@@ -1,5 +1,15 @@
 package com.thoughtworks.aceleradora.oo.carros;
 
 public class Carro {
+//    * Adicione à classe carro o atributo "marca" do tipo "String"
+//    * Adicione à classe carro o atributo "modelo" do tipo "String"
+//    * Adicione à classe carro o atributo "ano" do tipo "int"
+    private String marca;
+    private String modelo;
+    private int ano;
 
+
+    public String getMarca(){
+        return marca;
+    }
 }
