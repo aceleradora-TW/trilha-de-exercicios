1463ce883ae2000f57c0b87cda441992a5570ed8
nicoledbianchin <jeferson.cruz.n@hotmail.com>
2019-09-12 16:02:44 -0300

    dojo-16-agil/@todos: Começa o exercicio da Biblioteca

--- src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Enciclopedia.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Enciclopedia.java
@@ -9,7 +9,78 @@ public class Enciclopedia {
 
     public Enciclopedia() {
         // Adicione os livros aqui
+
+//
+        Livro livro0 = new Livro();
+        Autor autor0 = new Autor("Chimamanda","Adichie");
+        livro0.setAnoDeLancamento(2014);
+        livro0.setTitulo("Sejamos todos feministas");
+        livro0.adicionarFormato(Formato.EBOOK);
+        livro0.adicionarAutor(autor0);
+        livros.add(livro0);
+
+        Livro livro1= new Livro();
+        Autor autor1= new Autor("Alejandro", "Olchik");
+        livro1.setAnoDeLancamento(2017);
+        livro1.setTitulo("Management 3.0");
+        livro1.adicionarFormato(Formato.PDF);
+        livro1.adicionarFormato(Formato.FISICO);
+        livro1.adicionarAutor(autor1);
+        livros.add(livro1);
+
+        Livro livro2= new Livro();
+        Autor autor2= new Autor("George", "Orwell");
+        livro2.setAnoDeLancamento(1948);
+        livro2.setTitulo("1984");
+        livro2.adicionarFormato(Formato.PDF);
+        livro2.adicionarFormato(Formato.FISICO);
+        livro2.adicionarFormato(Formato.EBOOK);
+        livro2.adicionarAutor(autor2);
+        livros.add(livro2);
+
+        Livro livro3 = new Livro();
+        Autor autor3 = new Autor("Julia", "Naomi");
+        livro3.setAnoDeLancamento(2017);
+        livro3.adicionarAutor(autor3);
+        livro3.setTitulo("Lean Game Development");
+        livro3.adicionarFormato(Formato.PDF);
+        livro3.adicionarFormato(Formato.EBOOK);
+        livros.add(livro3);
+
+        Livro livro4 = new Livro();
+        Autor autor4 = new Autor("Eduardo","Galeano");
+        livro4.setAnoDeLancamento(1971);
+        livro4.setTitulo("Las venas abiertas de Latinoamerica");
+        livro4.adicionarFormato(Formato.FISICO);
+        livro4.adicionarFormato(Formato.PDF);
+        livro4.adicionarFormato(Formato.EBOOK);
+        livro4.adicionarAutor(autor4);
+        livros.add(livro4);
+
+        Livro livro5 = new Livro();
+        Autor autor5 = new Autor("Kathy","Sierra");
+        Autor autor51 = new Autor("Elizabeth","Freeman");
+        livro5.setAnoDeLancamento(2004);
+        livro5.setTitulo("Use a cabeça: Padrões de projeto");
+        livro5.adicionarFormato(Formato.FISICO);
+        livro5.adicionarFormato(Formato.PDF);
+        livro5.adicionarAutor(autor5);
+        livro5.adicionarAutor(autor51);
+        livros.add(livro5);
+
+        Livro livro6 = new Livro();
+        livro6.setAnoDeLancamento(2013);
+        livro6.setTitulo("Americanah");
+        livro6.adicionarFormato(Formato.EBOOK);
+        livro6.adicionarFormato(Formato.PDF);
+        livro6.adicionarFormato(Formato.FISICO);
+        livro6.adicionarAutor(autor0);
+        livros.add(livro6);
+
     }
+
+
+
 
     public String tituloDoEnesimoLivro(int posicao) {
         return null;
