49d9a4603a2104bf0f175903dec2bf5d9b3a85f8
gabreRabelo <gabriel.almeida97@gmail.com>
2019-09-23 11:09:04 -0300

    dupla06/@gabrieRabelo, @BrayanArgradem: Resolve o exercício IdentificadorDeArmstrong

--- src/main/java/com/thoughtworks/aceleradora/logica/IdentificadorDeArmstrong.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/IdentificadorDeArmstrong.java
@@ -1,8 +1,22 @@
 package com.thoughtworks.aceleradora.logica;
 
+import java.util.ArrayList;
+
 public class IdentificadorDeArmstrong {
 
     public String identificaNumero(int numero) {
-        return "Eu ainda nao sei identificar numeros de Armstrong";
+        String numerosString = Integer.toString(numero);
+        String [] numeros = numerosString.split("");
+        int tamanho = numeros.length;
+        int soma = 0;
+        for(int i = 0; i < tamanho; i++) {
+            int num = Integer.parseInt(numeros[i]);
+            soma = soma + (int)Math.pow(num, tamanho);
+        }
+        if(soma == numero){
+            return "Este eh um numero de Armstrong!";
+        }
+
+        return "Este nao eh um numero de Armstrong!";
     }
 }
