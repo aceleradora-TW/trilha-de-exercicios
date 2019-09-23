0216fb7d1cd0bf4552e793257388802ea6dc251b
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-09-23 10:59:53 -0300

    @stefanikopp/exercicio contador de ocorrencia

--- src/main/java/com/thoughtworks/aceleradora/logica/ContadorDeOcorrencias.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/ContadorDeOcorrencias.java
@@ -3,7 +3,16 @@ package com.thoughtworks.aceleradora.logica;
 public class ContadorDeOcorrencias {
 
     public int contaOcorrencias(String texto, String token) {
-        return -1;
+
+        String[] vetTexto = texto.split("");
+        int cont = 0;
+
+        for (int i = 0; i < vetTexto.length; i++){
+            if (vetTexto[i].equals(token)) {
+                cont++;
+            }
+        }
+        return cont;
     }
 
 }
