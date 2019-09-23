1389155b812f05c5e148a79c5ebff6cdb1749b54
cassia11 <cassiagomes0102@gmail.com>
2019-09-23 13:17:59 -0300

    dupla04/@adrio,@cassia11:faz exercicio de rpg

--- src/main/java/com/thoughtworks/aceleradora/oo/rpg/CriadorDePersonagens.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/rpg/CriadorDePersonagens.java
@@ -3,11 +3,45 @@ package com.thoughtworks.aceleradora.oo.rpg;
 public class CriadorDePersonagens {
 
     public Personagem criarPersonagemDeDefesa() {
-        return null;
+
+        Personagem personagem= new Personagem();
+        personagem.setNome("Lauri, o místico");
+        personagem.setForca(50);
+        personagem.setAgilidade(30);
+        personagem.setCarisma(2);
+        personagem.setConcentracao(10);
+
+        Arma arma= new Arma();
+        arma.setNome("Faca de cozinha enferrujada");
+        arma.setBonusDeAtaque(30);
+
+        Armadura armadura= new Armadura();
+        armadura.setNome("Armadura de Couro de Capivara");
+        armadura.setBonusDeDefesa(50);
+
+        personagem.setArma(arma);
+        personagem.setArmadura(armadura);
+        return personagem;
     }
 
     public Personagem criarPersonagemDeAtaque() {
-        return null;
-    }
+        Personagem personagem= new Personagem();
+        personagem.setNome("Romilda, a destruidora");
+        personagem.setForca(150);
+        personagem.setAgilidade(10);
+        personagem.setCarisma(200);
+        personagem.setConcentracao(10);
 
+        Arma arma= new Arma();
+        arma.setNome("Facão três listras lendário");
+        arma.setBonusDeAtaque(530);
+
+        Armadura armadura= new Armadura();
+        armadura.setNome("Armadura de folha de bananeira reforçada");
+        armadura.setBonusDeDefesa(100);
+
+        personagem.setArma(arma);
+        personagem.setArmadura(armadura);
+        return personagem;
+    }
 }
