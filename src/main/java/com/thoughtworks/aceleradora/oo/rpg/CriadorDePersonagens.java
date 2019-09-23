054a0af605bea22594440031dae154fd4af6955f
FranciscoGFerreira <franciscogabriellferreira@gmail.com>
2019-09-23 13:20:46 -0300

    dupla01/@EvertonFernandes93 @FranciscoGFerreira: faz o exercicio RPG em POO

--- src/main/java/com/thoughtworks/aceleradora/oo/rpg/CriadorDePersonagens.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/rpg/CriadorDePersonagens.java
@@ -3,11 +3,50 @@ package com.thoughtworks.aceleradora.oo.rpg;
 public class CriadorDePersonagens {
 
     public Personagem criarPersonagemDeDefesa() {
-        return null;
+
+        Personagem defender = new Personagem();
+
+        Arma weapon = new Arma();
+        weapon.setNome("Faca de cozinha enferrujada");
+        weapon.setBonusDeAtaque(30);
+
+        Armadura armor = new Armadura();
+        armor.setNome("Armadura de Couro de Capivara");
+        armor.setBonusDeDefesa(50);
+
+        defender.setNome("Lauri, o místico");
+        defender.setForca(50);
+        defender.setAgilidade(30);
+        defender.setCarisma(2);
+        defender.setConcentracao(10);
+        defender.setArma(weapon);
+        defender.setArmadura(armor);
+
+        return defender;
     }
 
     public Personagem criarPersonagemDeAtaque() {
-        return null;
+
+        Personagem avanger = new Personagem();
+
+        Arma weapon = new Arma();
+        weapon.setNome("Facão três listras lendário");
+        weapon.setBonusDeAtaque(530);
+
+        Armadura armor = new Armadura();
+        armor.setNome("Armadura de folha de bananeira reforçada");
+        armor.setBonusDeDefesa(100);
+
+        avanger.setNome("Romilda, a destruidora");
+        avanger.setForca(150);
+        avanger.setAgilidade(10);
+        avanger.setCarisma(200);
+        avanger.setConcentracao(10);
+        avanger.setArma(weapon);
+        avanger.setArmadura(armor);
+
+        return avanger;
+
     }
 
 }
