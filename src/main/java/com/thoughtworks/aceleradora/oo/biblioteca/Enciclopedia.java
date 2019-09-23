8db6a17a4808aa2cb401b553b527d0fa1d4dc717
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-09-23 13:28:18 -0300

    @stefanikpp/ oo - inicia exercicio biblioteca

--- src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Enciclopedia.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Enciclopedia.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.aceleradora.oo.biblioteca;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,22 @@ public class Enciclopedia {
     private List<Livro> livros = new ArrayList<>();
 
     public Enciclopedia() {
-        // Adicione os livros aqui
+
+        Livro livro0 = new Livro();
+        Autor autor0 = new Autor("Chimamanda", "Adichie");
+        livro0.adicionarAutor(autor0);
+        livro0.setTitulo("Sejamos tooos feministas");
+        livro0.setAnoDeLancamento(2014);
+        livro0.adicionarFormato(Formato.EBOOK);
+
+        Livro livro1 = new Livro();
+        Autor autor1 = new Autor("Alejandro", "Olchik");
+        livro1.adicionarAutor(autor1);
+        livro1.setTitulo("Management 3.0");
+        livro1.setAnoDeLancamento(2017);
+        livro1.adicionarFormato(Formato.PDF);
+        livro1.adicionarFormato(Formato.FISICO);
+
     }
 
     public String tituloDoEnesimoLivro(int posicao) {
--- src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Livro.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Livro.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.aceleradora.oo.biblioteca;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Livro {
@@ -10,12 +11,39 @@ public class Livro {
     private List<Formato> formatos;
 
     public Livro() {
-        // instancie as listas de formatos e autores aqui
+        List<Autor> listaAutores = new ArrayList<>();
+        List<Formato> listaFormatos = new ArrayList<>();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public List<Formato> getFormatos() {
+        return formatos;
     }
 
     public void adicionarFormato(Formato formato) {
+        this.formatos.add(formato);
     }
 
     public void adicionarAutor(Autor autor) {
+        this.autores.add(autor);
     }
 }
