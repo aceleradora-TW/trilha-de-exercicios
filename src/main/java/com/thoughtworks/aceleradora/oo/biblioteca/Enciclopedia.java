49c56387270fd8073f6e53c39d159638269efb31
EduardaSchuler <dudaschuler2011@gmail.com>
2019-09-23 14:48:08 -0300

    03/@EduardaSchuler, @JefersonCruz3: Faz exercicio Biblioteca

--- src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Enciclopedia.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Enciclopedia.java
@@ -8,27 +8,131 @@ public class Enciclopedia {
     private List<Livro> livros = new ArrayList<>();
 
     public Enciclopedia() {
-        // Adicione os livros aqui
+
+        Livro livro = new Livro();
+        Autor autor = new Autor("Chimamanda", "Adichie");
+
+        livro.adicionarAutor(autor);
+        livro.setTitulo("Sejamos todos feministas");
+        livro.setAnoDeLancamento(2014);
+        livro.adicionarFormato(Formato.EBOOK);
+        livros.add(livro);
+
+        livro = new Livro();
+        autor = new Autor("Alejandro", "Olchik");
+
+        livro.adicionarAutor(autor);
+        livro.setTitulo("Management 3.0");
+        livro.setAnoDeLancamento(2017);
+        livro.adicionarFormato(Formato.PDF);
+        livro.adicionarFormato(Formato.FISICO);
+        livros.add(livro);
+
+        livro = new Livro();
+        autor = new Autor("George", "Orwell");
+
+        livro.adicionarAutor(autor);
+        livro.setTitulo("1984");
+        livro.setAnoDeLancamento(1948);
+        livro.adicionarFormato(Formato.PDF);
+        livro.adicionarFormato(Formato.FISICO);
+        livro.adicionarFormato(Formato.EBOOK);
+        livros.add(livro);
+
+        livro = new Livro();
+        autor = new Autor("Julia", "Naomi");
+
+        livro.adicionarAutor(autor);
+        livro.setTitulo("Lean Game Development");
+        livro.setAnoDeLancamento(2017);
+        livro.adicionarFormato(Formato.PDF);
+        livro.adicionarFormato(Formato.EBOOK);
+        livros.add(livro);
+
+        livro = new Livro();
+        autor = new Autor("Eduardo", "Galeano");
+
+        livro.adicionarAutor(autor);
+        livro.setTitulo("Las venas abiertas de Latinoamerica");
+        livro.setAnoDeLancamento(1971);
+        livro.adicionarFormato(Formato.FISICO);
+        livro.adicionarFormato(Formato.PDF);
+        livro.adicionarFormato(Formato.EBOOK);
+        livros.add(livro);
+
+        livro = new Livro();
+        autor = new Autor("Kathy", "Sierra");
+
+        livro.adicionarAutor(autor);
+        autor = new Autor("Elizabeth","Freeman");
+
+        livro.adicionarAutor(autor);
+
+        livro.setTitulo("Use a cabeça: Padrões de projeto");
+        livro.setAnoDeLancamento(2004);
+        livro.adicionarFormato(Formato.FISICO);
+        livro.adicionarFormato(Formato.PDF);
+        livros.add(livro);
+
+        livro = new Livro();
+        autor = new Autor("Chimamanda", "Adichie");
+
+        livro.adicionarAutor(autor);
+        livro.setTitulo("Americanah");
+        livro.setAnoDeLancamento(2013);
+        livro.adicionarFormato(Formato.EBOOK);
+        livro.adicionarFormato(Formato.PDF);
+        livro.adicionarFormato(Formato.FISICO);
+        livros.add(livro);
+
     }
 
     public String tituloDoEnesimoLivro(int posicao) {
-        return null;
+        return livros.get(posicao).getTitulo();
     }
 
     public List<Livro> livrosDisponiveisEmPdf() {
-        return null;
+        List<Livro> livrosEmPdf = new ArrayList<>();
+        for (Livro livroAux: livros) {
+            for (Formato formatoPdf: livroAux.getFormatos()) {
+                if(formatoPdf.equals(Formato.PDF)){
+                    livrosEmPdf.add(livroAux);
+                }
+            }
+        }
+        return livrosEmPdf;
     }
 
     public List<Livro> buscaPorAutora(String nomeCompleto) {
-        return null;
+        List<Livro> listaChimamanda = new ArrayList<>();
+        for (Livro livroAux: livros) {
+            for (Autor autora: livroAux.getAutores()) {
+                if(autora.toString().equals(nomeCompleto)){
+                    listaChimamanda.add(livroAux);
+                }
+            }
+        }
+        return listaChimamanda;
     }
 
     public List<Livro> buscaPorAnoDeLancamento(int ano) {
-        return null;
+        List<Livro> livrosPorAno = new ArrayList<>();
+        for (Livro livroAux: livros) {
+           if(livroAux.getAnoDeLancamento() == ano){
+               livrosPorAno.add(livroAux);
+           }
+        }
+        return livrosPorAno;
     }
 
     public List<Livro> buscaPorPeriodoDeLancamento(int inicio, int fim) {
-        return null;
+        List<Livro> livrosPorPeriodo = new ArrayList<>();
+        for (Livro livroAux: livros) {
+            int anoLivro = livroAux.getAnoDeLancamento();
+            if(anoLivro >= inicio && anoLivro <= fim){
+                livrosPorPeriodo.add(livroAux);
+            }
+        }
+        return livrosPorPeriodo;
     }
-
 }
--- src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Livro.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Livro.java
@@ -1,21 +1,66 @@
 package com.thoughtworks.aceleradora.oo.biblioteca;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Livro {
 
     private String titulo;
     private int anoDeLancamento;
-    private List<Autor> autores;
-    private List<Formato> formatos;
+    private List<Autor> autores = new ArrayList<Autor>();
+    private List<Formato> formatos = new ArrayList<Formato>();
 
     public Livro() {
-        // instancie as listas de formatos e autores aqui
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(List<Autor> autores) {
+        this.autores = autores;
+    }
+
+    public List<Formato> getFormatos() {
+        return formatos;
+    }
+
+    public void setFormatos(List<Formato> formatos) {
+        this.formatos = formatos;
     }
 
     public void adicionarFormato(Formato formato) {
+        for (Formato newFormato: formatos) {
+            if (newFormato.equals(formato)){
+                return;
+            }
+        }
+        formatos.add(formato);
     }
 
     public void adicionarAutor(Autor autor) {
+        for (Autor newAutor: autores) {
+            if (newAutor.equals(autor)){
+                return;
+            }
+        }
+        autores.add(autor);
     }
+
 }
