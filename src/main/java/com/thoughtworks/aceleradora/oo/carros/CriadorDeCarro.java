97c148849153359630d7cfcda7a65d6dc73766fb
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-09-23 11:38:31 -0300

    @stefanikopp/oo - faz exercicios criador de carro

--- src/main/java/com/thoughtworks/aceleradora/oo/carros/CriadorDeCarro.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/carros/CriadorDeCarro.java
@@ -1,8 +1,17 @@
 package com.thoughtworks.aceleradora.oo.carros;
 
 public class CriadorDeCarro {
-
+/*
+  * A marca do carro deve ser "Ford"
+  * O modelo do carro deve ser "Mustang"
+  * O ano do carro deve ser "2017"*/
     public Carro criaUmCarro() {
-        return null;
+
+        Carro ford = new Carro();
+        ford.setAno(2017);
+        ford.setMarca("Ford");
+        ford.setModelo("Mustang");
+
+        return ford;
     }
 }
