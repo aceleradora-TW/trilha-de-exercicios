9fd60641804a166fc84e4d2058eeccab3cc96884
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-09-23 10:51:09 -0300

    02/ ingrid, rafaela: Resolve exercício do Carro.

--- src/main/java/com/thoughtworks/aceleradora/oo/carros/Carro.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/carros/Carro.java
@@ -1,5 +1,34 @@
 package com.thoughtworks.aceleradora.oo.carros;
 
 public class Carro {
+    private String modelo;
+    private String marca;
+    private int ano;
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getAno(){
+        return ano;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+
+    }
+
+    public void setAno(int ano ){
+        this.ano = ano;
+    }
 
 }
