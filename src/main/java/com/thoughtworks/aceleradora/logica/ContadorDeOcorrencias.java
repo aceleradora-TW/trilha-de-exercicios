851cb176f0230e3b4c5a81f137325ef7e32e30a1
cassia11 <cassiagomes0102@gmail.com>
2019-09-23 12:31:00 -0300

    dupla04/@cassia11, @adrio: faz contador de ocorrencia

--- src/main/java/com/thoughtworks/aceleradora/logica/ContadorDeOcorrencias.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/ContadorDeOcorrencias.java
@@ -3,7 +3,13 @@ package com.thoughtworks.aceleradora.logica;
 public class ContadorDeOcorrencias {
 
     public int contaOcorrencias(String texto, String token) {
-        return -1;
+        int cont = 0;
+        for (int i = 0; i < texto.length(); i++) {
+            if (token.charAt(0) == texto.charAt(i)) {
+                cont++;
+            }
+        }
+        return cont;
     }
 
 }
