0303859306afd3733ef19ed348e31d602e3abb71
cassia11 <cassiagomes0102@gmail.com>
2019-09-23 10:43:34 -0300

    dupla04/@adrio,@cassia11: faz exercicio FizzBuzz

--- src/main/java/com/thoughtworks/aceleradora/logica/TradutorFizzBuzz.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/TradutorFizzBuzz.java
@@ -1,8 +1,23 @@
 package com.thoughtworks.aceleradora.logica;
 
+import java.util.Objects;
+
 public class TradutorFizzBuzz {
 
     String traduz(int numero) {
-        return null;
+
+        if (numero % 3 == 0 && numero % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (numero % 3 == 0){
+            return "Fizz";
+        }
+        if (numero % 5 == 0){
+            return "Buzz";
+        }
+
+         String outroNumero = Objects.toString(numero);
+        return outroNumero;
     }
+
 }
