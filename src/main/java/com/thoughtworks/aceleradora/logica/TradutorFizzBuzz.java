32105746b695d75a865c9ccff56fc5dbdcfffee1
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-09-23 10:40:31 -0300

    exercicio fizzbuzz

--- src/main/java/com/thoughtworks/aceleradora/logica/TradutorFizzBuzz.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/TradutorFizzBuzz.java
@@ -2,7 +2,20 @@ package com.thoughtworks.aceleradora.logica;
 
 public class TradutorFizzBuzz {
 
+
     String traduz(int numero) {
-        return null;
+
+        if (numero%3 == 0 && numero%5 == 0) {
+            return "FizzBuzz";
+        }
+        if (numero%3 == 0) {
+            return "Fizz";
+        }
+        if (numero%5 == 0) {
+            return "Buzz";
+        }
+        return Integer.toString(numero);
+
     }
+
 }
