28feae829c6353816bc423cabf94ca21e2598821
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-09-23 15:48:54 -0300

    02/@IngridDuarte, @Queiroxzz: Implementa a lógica enciclopedia

--- src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Enciclopedia.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Enciclopedia.java
@@ -8,11 +8,79 @@ public class Enciclopedia {
     private List<Livro> livros = new ArrayList<>();
 
     public Enciclopedia() {
-        // Adicione os livros aqui
+
+        Livro adichie = new Livro();
+        Autor chimamanda = new Autor("Chimamanda", "Adichie");
+        adichie.setTitulo("Sejamos todos feministas");
+        adichie.setAnoDeLancamento(2014);
+        adichie.adicionarAutor(chimamanda);
+        adichie.adicionarFormato(Formato.EBOOK);
+
+        Livro olchick = new Livro();
+        Autor alejandro = new Autor("Alejandro", "Olchick");
+        olchick.setTitulo("Management 3.0");
+        olchick.setAnoDeLancamento(2017);
+        olchick.adicionarAutor(alejandro);
+        olchick.adicionarFormato(Formato.PDF);
+        olchick.adicionarFormato(Formato.FISICO);
+
+        Livro orwell = new Livro();
+        Autor george = new Autor("George", "Orwell");
+        orwell.setTitulo("1984");
+        orwell.setAnoDeLancamento(1948);
+        orwell.adicionarAutor(george);
+        orwell.adicionarFormato(Formato.PDF);
+        orwell.adicionarFormato(Formato.FISICO);
+        orwell.adicionarFormato(Formato.EBOOK);
+
+        Livro naomi = new Livro();
+        Autor julia = new Autor("Julia", "Naomi");
+        naomi.setTitulo("Lean Game Development");
+        naomi.setAnoDeLancamento(2017);
+        naomi.adicionarAutor(julia);
+        naomi.adicionarFormato(Formato.PDF);
+        naomi.adicionarFormato(Formato.EBOOK);
+
+        Livro galeano = new Livro();
+        Autor eduardo = new Autor("Eduardo", "Galeano");
+        galeano.setTitulo("Las venas abiertas de Latinoamerica");
+        galeano.setAnoDeLancamento(1971);
+        galeano.adicionarAutor(eduardo);
+        galeano.adicionarFormato(Formato.FISICO);
+        galeano.adicionarFormato(Formato.PDF);
+        galeano.adicionarFormato(Formato.EBOOK);
+
+        Livro sierraEhFreeman = new Livro();
+        Autor kathy = new Autor("Kathy", "Sierra");
+        Autor elizabeth = new Autor("Elizabeth", "Freeman");
+        sierraEhFreeman.setTitulo("Use a cabeça: Padrões de projeto");
+        sierraEhFreeman.setAnoDeLancamento(2004);
+        sierraEhFreeman.adicionarAutor(kathy);
+        sierraEhFreeman.adicionarAutor(elizabeth);
+        sierraEhFreeman.adicionarFormato(Formato.FISICO);
+        sierraEhFreeman.adicionarFormato(Formato.PDF);
+
+        Livro adichie02 = new Livro();
+        adichie02.setTitulo("Americanah");
+        adichie02.setAnoDeLancamento(2013);
+        adichie02.adicionarAutor(chimamanda);
+        adichie02.adicionarFormato(Formato.EBOOK);
+        adichie02.adicionarFormato(Formato.PDF);
+        adichie02.adicionarFormato(Formato.FISICO);
+
+        livros.add(adichie);
+        livros.add(olchick);
+        livros.add(orwell);
+        livros.add(naomi);
+        livros.add(galeano);
+        livros.add(sierraEhFreeman);
+        livros.add(adichie02);
     }
 
+
+
     public String tituloDoEnesimoLivro(int posicao) {
-        return null;
+        return livros.get(posicao).getTitulo();
     }
 
     public List<Livro> livrosDisponiveisEmPdf() {
--- src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Livro.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/biblioteca/Livro.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.aceleradora.oo.biblioteca;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Livro {
@@ -10,12 +11,45 @@ public class Livro {
     private List<Formato> formatos;
 
     public Livro() {
-        // instancie as listas de formatos e autores aqui
+        autores = new ArrayList<>();
+        formatos = new ArrayList<>();
+
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public List<Formato> getFormatos() {
+        return formatos;
     }
 
     public void adicionarFormato(Formato formato) {
+        formatos.add(formato);
+
     }
 
     public void adicionarAutor(Autor autor) {
+        autores.add(autor);
+
     }
+
+
 }
