4f8a552e3778f82ad613dcedf75efd3572d131c1
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2019-09-23 11:46:46 -0300

    02/ @ingridDuarte, @Queiroxzz: Resolve exercício Soma Dobrada

--- src/main/java/com/thoughtworks/aceleradora/logica/SomaDobrada.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/SomaDobrada.java
@@ -3,6 +3,19 @@ package com.thoughtworks.aceleradora.logica;
 public class SomaDobrada {
 
     public int soma(int umValor, int outroValor) {
-       return -500;
+
+//        Caso os dois números recebidos sejam iguais, o método retorna o dobro da soma;
+//        Caso nenhuma das condições anteriores seja verdadeira, o método retorna a soma dos dois números.
+        int somaDosValores = umValor + outroValor;
+
+        if(umValor < 1 || outroValor < 1) {
+              return -1;
+          }
+
+          if(umValor == outroValor) {
+              return somaDosValores*2;
+          }
+
+        return somaDosValores;
     }
 }
