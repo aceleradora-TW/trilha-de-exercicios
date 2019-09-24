ada3768d6c8b8e3ebfd2fb99603432a71ebe7a60
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-09-24 09:48:44 -0300

    dupla05/@stefanikopp: faz exercicio personagem rpg

--- src/main/java/com/thoughtworks/aceleradora/oo/rpg/CriadorDePersonagens.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/rpg/CriadorDePersonagens.java
@@ -3,11 +3,46 @@ package com.thoughtworks.aceleradora.oo.rpg;
 public class CriadorDePersonagens {
 
     public Personagem criarPersonagemDeDefesa() {
-        return null;
+
+        Personagem personagemDefesa = new Personagem();
+        personagemDefesa.setNome("Lauri, o místico");
+        personagemDefesa.setForca(50);
+        personagemDefesa.setAgilidade(30);
+        personagemDefesa.setCarisma(2);
+        personagemDefesa.setConcentracao(10);
+
+        Arma armaDefesa = new Arma();
+        armaDefesa.setNome("Faca de cozinha enferrujada");
+        armaDefesa.setBonusDeAtaque(30);
+
+        Armadura armaduraDefesa = new Armadura();
+        armaduraDefesa.setNome("Armadura de Couro de Capivara");
+        armaduraDefesa.setBonusDeDefesa(50);
+
+        personagemDefesa.setArma(armaDefesa);
+        personagemDefesa.setArmadura(armaduraDefesa);
+        return personagemDefesa;
     }
 
     public Personagem criarPersonagemDeAtaque() {
-        return null;
+        Personagem personagemAtaque = new Personagem();
+        personagemAtaque.setNome("Romilda, a destruidora");
+        personagemAtaque.setForca(150);
+        personagemAtaque.setAgilidade(10);
+        personagemAtaque.setCarisma(200);
+        personagemAtaque.setConcentracao(10);
+
+        Arma armaAtaque = new Arma();
+        armaAtaque.setNome("Facão três listras lendário");
+        armaAtaque.setBonusDeAtaque(530);
+
+        Armadura armaduraAtaque = new Armadura();
+        armaduraAtaque.setNome("Armadura de folha de bananeira reforçada");
+        armaduraAtaque.setBonusDeDefesa(100);
+
+        personagemAtaque.setArma(armaAtaque);
+        personagemAtaque.setArmadura(armaduraAtaque);
+        return personagemAtaque;
     }
 
 }
