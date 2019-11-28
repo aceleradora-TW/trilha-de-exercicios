7485db27f5afc270628c53b7865c8247bcc9dab7
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-28 01:33:51 -0300

    refatora nome de métodos do interpretador

--- src/main/java/com/thoughtworks/aceleradora/oo/conversor/InterpretadorDeEntrada.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/conversor/InterpretadorDeEntrada.java
@@ -1,22 +1,21 @@
 package com.thoughtworks.aceleradora.oo.conversor;
 
 public class InterpretadorDeEntrada {
-    public String entrada;
     public String moedaInicial;
     public String moedaFinal;
     public Double valor;
 
-    public String verificarMoedaEntrada(String entrada){
+    public String extrairMoedaEntrada(String entrada){
         this.moedaInicial = entrada.substring(0, 3);
         return moedaInicial;
     }
 
-    public Double verificarValor(String entrada){
+    public Double extrairValor(String entrada){
         this.valor = Double.parseDouble(entrada.replaceAll("[^0-9]*", ""));
         return valor;
     }
 
-    public String verificarMoedaDesejada(String entrada){
+    public String extrairMoedaDesejada(String entrada){
         this.moedaFinal = entrada.split(" ")[1];
         return moedaFinal;
     }
