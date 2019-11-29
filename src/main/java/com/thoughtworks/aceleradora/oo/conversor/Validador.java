815dd50bd03016c10b8eca2a35138be2b250a413
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-29 00:36:48 -0300

    adiciona validação para moeda de saida

--- src/main/java/com/thoughtworks/aceleradora/oo/conversor/Validador.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/conversor/Validador.java
@@ -3,14 +3,25 @@ package com.thoughtworks.aceleradora.oo.conversor;
 public class Validador {
     InterpretadorDeEntrada interpretadorDeEntrada;
     String moedaInicial;
+    String moedaFinal;
 
-    public String verificarMoedaValida(String entrada){
-        this.moedaInicial =  interpretadorDeEntrada.moedaInicial;
+    public String validarMoedaInicial(String entrada){
+        this.moedaInicial =  interpretadorDeEntrada.extrairMoedaEntrada(entrada);
         for (Moedas moeda : Moedas.values()){
             if (moedaInicial.equals(moeda)){
                 return moedaInicial;
             }
         }
         return "Moeda nao suportada: " + moedaInicial;
+    }
+
+    public String validarMoedaFinal(String entrada){
+        this.moedaFinal =  interpretadorDeEntrada.extrairMoedaDesejada(entrada);
+        for (Moedas moeda : Moedas.values()){
+            if (moedaFinal.equals(moeda)){
+                return moedaFinal;
+            }
+        }
+        return "Moeda nao suportada: " + moedaFinal;
     }
 }
