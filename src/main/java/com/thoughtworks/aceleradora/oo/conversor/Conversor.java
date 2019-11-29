9979ce73973477db37141c6300288e8b0a5fe3b4
nicoledbianchin <nickdiasbianchin@hotmail.com>
2019-11-29 01:10:20 -0300

    estrutura classe conversor

--- src/main/java/com/thoughtworks/aceleradora/oo/conversor/Conversor.java
+++ src/main/java/com/thoughtworks/aceleradora/oo/conversor/Conversor.java
@@ -2,7 +2,48 @@ package com.thoughtworks.aceleradora.oo.conversor;
 
 public class Conversor {
 
+    InterpretadorDeEntrada interpretadorDeEntrada;
+    Validador validador;
+    TaxaDeCambio taxaDeCambio;
+    CalculadoraDeCambio calculadoraDeCambio;
+
+    String moedaInicial;
+    String moedaFinal;
+    Double valor;
+    Double taxaDeCambioAtual;
+    Double resultado;
+    String moedaParaConversao;
+    Boolean validacaoMoedaInicial;
+    Boolean validacaoMoedaFinal;
+
+    public Conversor(InterpretadorDeEntrada interpretadorDeEntrada, Validador validador, TaxaDeCambio taxaDeCambio){
+        this.interpretadorDeEntrada = interpretadorDeEntrada;
+        this.validador = validador;
+        this.taxaDeCambio = taxaDeCambio;
+    }
+
+    public void extraiParametrosEValor(String entrada){
+        moedaInicial = interpretadorDeEntrada.extrairMoedaInicial(entrada);
+        moedaFinal = interpretadorDeEntrada.extrairMoedaFinal(entrada);
+        valor = interpretadorDeEntrada.extrairValor(entrada);
+        moedaParaConversao = interpretadorDeEntrada.detectarMoedasParaConversao(moedaInicial, moedaFinal);
+    }
+
     public String converter(String entrada) {
-       return null;
+        validacaoMoedaInicial = validador.validarMoedaInicial(moedaInicial);
+        validacaoMoedaFinal = validador.validarMoedaFinal(moedaFinal);
+
+        if (validacaoMoedaInicial == false){
+            return "Moeda nao suportada: " + moedaInicial;
+        }
+        if (validacaoMoedaFinal == false){
+            return "Moeda nao suportada: " + moedaFinal;
+        }
+        else{
+            taxaDeCambioAtual = calculadoraDeCambio.determinarTaxaDeCambio(moedaParaConversao);
+            resultado = calculadoraDeCambio.converterMoedas(taxaDeCambioAtual, valor);
+            return moedaFinal + resultado;
+        }
     }
 }
+
