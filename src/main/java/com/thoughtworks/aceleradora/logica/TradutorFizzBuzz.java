a57dd9451db7bc5d845158f5aa8274267819b459
ingridDuarte <ingrid.b.duarte.id.id@gmail.com>
2020-01-16 12:00:37 -0300

    Resolve exercicio FizzBuzz

--- src/main/java/com/thoughtworks/aceleradora/logica/TradutorFizzBuzz.java
+++ src/main/java/com/thoughtworks/aceleradora/logica/TradutorFizzBuzz.java
@@ -4,5 +4,16 @@ public class TradutorFizzBuzz {
 
     String traduz(int numero) {
         return null;
+        if(numero % 3 == 0 && numero
+        % 5 == 0){
+            return "FizzBuzz";
+        }
+        else if (numero % 3 == 0){
+            return "Fizz";
+        }
+        else if (numero % 5 == 0){
+            return "Buzz";
+        }
+        return Integer.toString(numero);
     }
 }
